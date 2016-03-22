7c8eb20bb777b894cc615cbb8fb6328cb7f2996e
burzyk <p.k.burzynski@gmail.com>
2016-03-22 10:46:29 +0100

    InMemoryRepository multi user option

--- src/main/scala/com/jpbnetsoftware/homebudget/data/InMemoryOperationsRepository.scala
+++ src/main/scala/com/jpbnetsoftware/homebudget/data/InMemoryOperationsRepository.scala
@@ -2,22 +2,28 @@ package com.jpbnetsoftware.homebudget.data
 
 import com.jpbnetsoftware.homebudget.domain.BankOperation
 
+import scala.collection.mutable.HashMap
+
 /**
   * Created by pburzynski on 21/03/2016.
   */
 class InMemoryOperationsRepository extends OperationsRepository {
 
-  var operations: List[BankOperation] = List[BankOperation]()
+  var operations: HashMap[Int, List[BankOperation]] = HashMap[Int, List[BankOperation]]()
 
   override def operationExists(userId: Int, operation: BankOperation): Boolean = {
-    operations.count(o => o == operation) != 0
+    operations.contains(userId) && operations(userId).count(o => o == operation) != 0
   }
 
   override def insertOperation(userId: Int, operation: BankOperation): Unit = {
-    operations = operation :: operations
+    if (!operations.contains(userId)) {
+      operations += (userId -> List[BankOperation]())
+    }
+
+    operations(userId) = operation :: operations(userId)
   }
 
   override def getOperations(userId: Int): List[BankOperation] = {
-    operations
+    if (!operations.contains(userId)) List[BankOperation]() else operations(userId)
   }
 }
